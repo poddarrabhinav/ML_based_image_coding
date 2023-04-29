9f0646852c5d966c05ff94f078f2c0a7eb63c115
TUdayKiranReddy <ee19btech11038@iith.ac.in>
2023-04-29 16:48:21 +0530

    pca template

--- ml_coding/PCA.py
+++ ml_coding/PCA.py
@@ -1,1 +1,29 @@
 # PCA based coding
+import numpy as np
+from sklearn.decomposition import PCA
+
+
+
+
+
+class PCA():
+    def __init__(self):
+        n_components = 100
+        compressor = PCA(n_components)
+
+    def learn(self, raw_datas):
+        compressor.fit(raw_datas)
+
+
+    def compress(self, raw_data):
+        return compressor.fit_transform(raw_data)
+
+    def decompress(self, data):
+        return compressor.inverse_transform(data)
+
+    def compress_multiple(self):
+        pass
+
+    def decompress_multiple(self):
+        pass
+
